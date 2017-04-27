1906abf6b9584a16c7d8b112ddd1cfbaa2112967
theitbrain <itbrainpost@gmail.com>
2017-04-27 22:31:07 +0900

    최대값과 최소값 문제 해결

--- 최소값과 최대값_2357/최소값과 최대값_2357/소스.cpp
+++ 최소값과 최대값_2357/최소값과 최대값_2357/소스.cpp
@@ -1,42 +1,108 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
+#define MAX 400001
 
+int rmq_min[MAX];
+int rmq_max[MAX];
 int d[100001];
+int N, M;
 
-int main()
+inline int min(int a, int b)
 {
-	int N, M;
+	return (a > b) ? b : a;
+}
+
+inline int max(int a, int b)
+{
+	return (a > b) ? a : b;
+}
+
+// node가 담당하는 범위가 left ~ right
+// node: 세그먼트 트리 노드 번호 
+int init_rmq_min(int left, int right, int node) 
+
+{
+	if (left == right)
+		return rmq_min[node] = d[left];
+	int mid = (left + right) / 2;
+	int leftMin = init_rmq_min(left, mid, node * 2);
+	int rightMin = init_rmq_min(mid + 1, right, node * 2 + 1);
+
+	return rmq_min[node] = min(leftMin, rightMin);
+}
+
+int init_rmq_max(int left, int right, int node)
+
+{
+	if (left == right)
+		return rmq_max[node] = d[left];
+	int mid = (left + right) / 2;
+	int leftMax = init_rmq_max(left, mid, node * 2);
+	int rightMax = init_rmq_max(mid + 1, right, node * 2 + 1);
+
+	return rmq_max[node] = max(leftMax, rightMax);
+}
+
+int query_rmq_min(int left, int right, int node_left, int node_right, int node)
+{
+	if (left > node_right || right < node_left)
+		return 1000000009;
+	
+	if (left <= node_left && node_right <= right)
+		return rmq_min[node];
+
+	int mid = (node_left + node_right) / 2;
+	int leftMin = query_rmq_min(left, right, node_left, mid, node * 2);
+	int rightMin = query_rmq_min(left, right, mid + 1, node_right, node * 2 + 1);
+
+	return min(leftMin, rightMin);
+}
+
+int query_rmq_max(int left, int right, int node_left, int node_right, int node)
+{
+	if (left > node_right || right < node_left)
+		return -1000000009;
+
+	if (left <= node_left && node_right <= right)
+		return rmq_max[node];
+
+	int mid = (node_left + node_right) / 2;
+	int leftMax = query_rmq_max(left, right, node_left, mid, node * 2);
+	int rightMax = query_rmq_max(left, right, mid + 1, node_right, node * 2 + 1);
+
+	return max(leftMax, rightMax);
+}
+
+int main(int argc, char** argv)
+{
 	std::ios::sync_with_stdio(false);
-	freopen("Text.txt", "r", stdin);
+	//freopen("Text.txt", "r", stdin);
 
 	cin >> N >> M;
+	//scanf("%d %d", &N, &M);
 
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++)
 		cin >> d[i];
-	}
+		//scanf("%d", &d[i]);
+		
 
-	for (int i = 1; i <= M; i++) {
-		int a, b;
-		int MIN, MAX;
-		cin >> a >> b;
+	init_rmq_min(0, N - 1, 1);
+	init_rmq_max(0, N - 1, 1);
 
-		if (d[a] <= d[a + 1]) {
-			MIN = d[a]; MAX = d[a + 1];
-		}
-		else {
-			MIN = d[a + 1]; MAX = d[a];
-		}
+	for (int i = 0; i < M; i++)
+	{
+		int queryLeft, queryRight;
+		cin >> queryLeft >> queryRight;
+		//scanf("%d %d", &queryLeft, &queryRight);
+		queryLeft--; queryRight--;
 
-		for (int j = a + 2; j <= b; j++) {
-			if (d[j] <= MIN)
-				MIN = d[j];
-			else if (d[j] >= MAX)
-				MAX = d[j];
-		}
-
-		cout << MIN << " " << MAX << endl;
+		//cout << query_rmq_min(queryLeft, queryRight, 0, N - 1, 1) << " " << query_rmq_max(queryLeft, queryRight, 0, N - 1, 1) << endl;
+		
+		printf("%d ", query_rmq_min(queryLeft, queryRight, 0, N - 1, 1));
+		printf("%d\n", query_rmq_max(queryLeft, queryRight, 0, N - 1, 1));
 	}
 
 
-	return 0;
+	return 0; // 정상종료시 반드시 0을 리턴해야 합니다.
 }
